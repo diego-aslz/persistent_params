2603a72b1b4038284a2fb1844893c1a7a5b29d56
Diego Aguir Selzlein <diego.selzlein@unila.edu.br>
2015-12-29 10:24:58 -0200

    Initial commit to persistent_params.

---
+++ test/test_persistent_params.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestPersistentParams < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
